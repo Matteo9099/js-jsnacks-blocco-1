300128281484f056deaa182d43fdab34dbbe51da
Matteo Morganti <matteo99morganti@gmail.com>
2022-01-27 16:28:48 +0100

    push

--- jssnack2/main.js
+++ jssnack2/main.js
@@ -7,7 +7,12 @@ let i = 0;
 while(i<utente){
     numero = parseInt(prompt('Inserire un numero'));
 
+    if(isNaN(utente) || isNaN(numero)){
+        alert('Inserire un numero valido!');
+    }
+
     console.log("Cubo di " + numero + ": " +Math.pow(numero, 3));
 
     i++;
 }
+
