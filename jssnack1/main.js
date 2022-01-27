31e5dfa345063ded9def5a294146f1d3a24f1019
Matteo Morganti <matteo99morganti@gmail.com>
2022-01-27 15:58:23 +0100

    aggiunto isNaN

--- jssnack1/main.js
+++ jssnack1/main.js
@@ -4,6 +4,10 @@ let somma = 0;
 for(let i=0; i<10; i++){
     numero = parseInt(prompt('Inserire un numero'));
 
+    if(isNaN(numero)){
+        alert('Inserisci un numero valido!');
+    }
+
     somma = parseInt(numero) + somma;
 
 }
---
+++ jssnack2/main.js
@@ -0,0 +1,1 @@
+// input utente scelta numero
